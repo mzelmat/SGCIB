06073b9dcc4eef17c72988b6b49161b3a230a379
mzelmat <mzelmat@gmail.com>
2018-02-26 10:42:58 +0100

    Update OperationServiceProxy.java

--- account-service/src/main/java/microservice/example/account/OperationServiceProxy.java
+++ account-service/src/main/java/microservice/example/account/OperationServiceProxy.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service")
-@RibbonClient(name="forex-service")
+@FeignClient(name="operation-service")
+@RibbonClient(name="operation-service")
 public interface OperationServiceProxy {
   @GetMapping("/operations/{accountNumber}/history")
   public List<AccountBean> getHistory(@PathVariable("accountNumber") long accountNumber);
